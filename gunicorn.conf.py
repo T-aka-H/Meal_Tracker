682ebdef69b8483e9933a79ff919f95b8cf97dda
T-aka-H <takayuki.hamuro@sumitomocorp.com>
2025-06-06 15:51:44 +0900

    1551

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,8 +1,8 @@
 import multiprocessing
 import os
 
-# ワーカープロセス数
-workers = multiprocessing.cpu_count() * 2 + 1
+# ワーカープロセス数（開発環境では1つで十分）
+workers = 1
 
 # ワーカークラス
 worker_class = 'sync'
@@ -10,21 +10,29 @@ worker_class = 'sync'
 # バインドするアドレス
 bind = f"0.0.0.0:{os.environ.get('PORT', '5000')}"
 
-# タイムアウト設定
-timeout = 120
+# タイムアウト設定（COHEREのAPIコールに十分な時間を確保）
+timeout = 300
 
 # アクセスログの設定
 accesslog = '-'
-access_log_format = '%({x-real-ip}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(L)s'
 
 # エラーログの設定
 errorlog = '-'
-loglevel = 'info'
+loglevel = 'debug'
 
 # プリロード
 preload_app = True
 
-# 最大リクエストサイズ
-limit_request_line = 0
-limit_request_fields = 32768
-limit_request_field_size = 0 
+# リクエスト設定
+max_requests = 1000
+max_requests_jitter = 50
+
+# タイムアウト設定
+graceful_timeout = 120
+keep_alive = 5
+
+# バッファサイズ設定
+limit_request_line = 8190
+limit_request_fields = 100
+limit_request_field_size = 8190 
